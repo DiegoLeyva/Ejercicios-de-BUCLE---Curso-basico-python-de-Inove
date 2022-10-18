ccf039104f4c4a3e54f1032e039f00357b6a4f4f
DiegoLeyva <69438026+DiegoLeyva@users.noreply.github.com>
2022-10-18 19:17:09 -0300

    Add files via upload

---
+++ ejercicio_6.py
@@ -0,0 +1,38 @@
+# Bucles [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicio de secuencias numéricas
+
+# Pedir por consola dos números que representen el principio y fin de una
+# secuencia numérica.
+# Realizar un bucle "for" que recorra esa secuencia armada con "range"
+# y cuante cuantes números son negativos y cuantos números son mayor o igual a cero
+# Tener en cuenta que "range" no incluye el número de "fin" en su secuencia,
+# sino que va hasta el anterior
+
+inicio = int(input('Ingrese el primer número de la secuencia\n'))
+fin = int(input('Ingrese el último número de la secuencia\n'))
+
+cantidad_numeros_positivos = 0  # Inicializo el contador en 0
+cantidad_numeros_negativos = 0
+cantidad_ceros = 0
+
+# for ... in range(....)
+for secuencia in range(inicio, fin, + 1):
+    if secuencia > 0:
+        cantidad_numeros_positivos += 1
+    elif secuencia < 0:
+        cantidad_numeros_negativos += 1
+    else:
+        cantidad_ceros += 1
+# Imprimir el valor de la cantidad de números positivos y negativos
+print('La cantidad de números positivos es: ', cantidad_numeros_positivos)
+print('La cantidad de números negativos es: ', cantidad_numeros_negativos)
+print('La cantidad de ceros es: ', cantidad_ceros)
+print("terminamos!")
